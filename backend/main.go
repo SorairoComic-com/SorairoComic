25a46e91e6a4fd29189c38ba85e3a54ea4eab899
nokatotedo <dwikypanduaqri22@gmail.com>
2023-12-27 23:06:06 +0700

    make login route

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"sorairocomic/router"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	app := gin.Default()
+	port := 3000
+
+	router.PostLogin(app.Group("/login"))
+
+	app.Run(fmt.Sprintf(":%d", port))
+	fmt.Println("http://localhost:3000/")
 }
