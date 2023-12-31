b049151ec90750e68856dd4fd34ab71e8b6092b5
nokatotedo <dwikypanduaqri22@gmail.com>
2024-01-01 03:07:13 +0700

    refactor login route

--- backend/types/users.go
+++ backend/types/users.go
@@ -1,13 +1,30 @@
 package types
 
-import "github.com/golang-jwt/jwt"
+import (
+	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
-type Login struct {
+type LoginBody struct {
+	Username string
+	Password string
+}
+
+type LoginResponse struct {
 	AccessToken string `json:"access_token"`
 }
 
+type User struct {
+	Id       primitive.ObjectID `bson:"_id"`
+	Username string
+	Password string
+}
+
 type Token struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Id primitive.ObjectID `json:"_id"`
 	jwt.StandardClaims
+}
+
+type Error struct {
+	Message string `json:"message"`
 }
